a252ea8384b5a7f6f98daa9e591349a45243c0d7
fineboy0407 <122990468+fineboy0407@users.noreply.github.com>
2023-02-03 21:25:06 +0900

    Add a signalR hub
    Co-authored-by: fineboy0407 <levilee.fine@gmail.com>
    Co-authored-by: fineboy0513 <dennisreynolds.happy@gmail.com>

---
+++ chatapp-blazor-server-signalR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chatapp_blazor_server_signalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+    }
+}
